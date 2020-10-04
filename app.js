0de29494e113a4d703f8e65966c8f3731341f0f7
hussainM <hussainm_zenos@hotmail.co.uk>
2020-10-04 10:43:55 +0100

    [AM]: Added functionality to the sell section slide show

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let slideIndex = 0;
 let slideIndexTwo = 0;
+let sellSlideIndex = 0;
 
 showSlides();
 showSlidesTwo();
+sellSlideShow();
 
 function showSlides(){
   var i;
@@ -26,4 +28,16 @@ function showSlidesTwo(){
    if(slideIndexTwo > slides.length){slideIndexTwo = 1}
   slides[slideIndexTwo - 1].style.display = 'block';
   setTimeout(showSlidesTwo, 5000);
+}
+
+function sellSlideShow(){
+  var i;
+  var slides = document.getElementsByClassName('sell-slide');
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  sellSlideIndex++;
+  if(sellSlideIndex > slides.length){sellSlideIndex = 1}
+  slides[sellSlideIndex -1].style.display = 'block';
+  setTimeout(sellSlideShow, 5000);
 }
