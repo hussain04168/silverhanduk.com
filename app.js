8b14576781bcee71eb21fabf19c4b67639a9b3d8
hussainM <hussainm_zenos@hotmail.co.uk>
2020-10-07 02:59:46 +0100

    [AM]: Removed functionality of sell section slide

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// slide show logic
+
 let slideIndex = 0;
 let slideIndexTwo = 0;
-let sellSlideIndex = 0;
+
 
 showSlides();
 showSlidesTwo();
-sellSlideShow();
+
 
 function showSlides(){
   var i;
@@ -28,16 +30,4 @@ function showSlidesTwo(){
    if(slideIndexTwo > slides.length){slideIndexTwo = 1}
   slides[slideIndexTwo - 1].style.display = 'block';
   setTimeout(showSlidesTwo, 5000);
-}
-
-function sellSlideShow(){
-  var i;
-  var slides = document.getElementsByClassName('sell-slide');
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  sellSlideIndex++;
-  if(sellSlideIndex > slides.length){sellSlideIndex = 1}
-  slides[sellSlideIndex -1].style.display = 'block';
-  setTimeout(sellSlideShow, 5000);
 }
