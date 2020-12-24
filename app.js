a3e5786eda2e7b9b496c413a6b140d012d668b09
hussain04168 <hussainm_zenos@hotmail.co.uk>
2020-12-24 10:08:13 +0000

    [AM]: Removed search feature from index.html

--- app.js
+++ app.js
@@ -2,73 +2,71 @@
 
 let slideIndex = 0;
 let slideIndexTwo = 0;
-let navButton = document.querySelector('.nav-btn');
-let navMenu = document.querySelector('#navMenu');
-let navCloseButton = document.querySelector('.nav-close-btn');
-let navList = document.querySelector('.navList');
-let closeSearchButon = document.querySelector('.close-search-btn');
-let searchBarDiv = document.querySelector('.search-bar');
-let searchButton = document.querySelector('.search-btn');
-let userInput = document.getElementById('userInput');
+let navButton = document.querySelector(".nav-btn");
+let navMenu = document.querySelector("#navMenu");
+let navCloseButton = document.querySelector(".nav-close-btn");
+let navList = document.querySelector(".navList");
+let closeSearchButon = document.querySelector(".close-search-btn");
+let searchBarDiv = document.querySelector(".search-bar");
+let searchButton = document.querySelector(".search-btn");
+let userInput = document.getElementById("userInput");
 
 // slide show logic
 showSlides();
 showSlidesTwo();
 
-function showSlides(){
+function showSlides() {
   var i;
-  var slides = document.getElementsByClassName('slide');
-   for(i = 0; i < slides.length; i++){
-     slides[i].style.display = 'none';
-   }
-   slideIndex++;
-   if(slideIndex > slides.length){slideIndex = 1}
-  slides[slideIndex - 1].style.display = 'block';
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5000);
 }
 
-function showSlidesTwo(){
+function showSlidesTwo() {
   var i;
-  var slides = document.getElementsByClassName('middle-slide-img');
-   for(i = 0; i < slides.length; i++){
-     slides[i].style.display = 'none';
-   }
-   slideIndexTwo++;
-   if(slideIndexTwo > slides.length){slideIndexTwo = 1}
-  slides[slideIndexTwo - 1].style.display = 'block';
+  var slides = document.getElementsByClassName("middle-slide-img");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndexTwo++;
+  if (slideIndexTwo > slides.length) {
+    slideIndexTwo = 1;
+  }
+  slides[slideIndexTwo - 1].style.display = "block";
   setTimeout(showSlidesTwo, 5000);
 }
 
-
 // Navigation logic
 
-function showNav(){
-  navMenu.style.transform = 'translateX(0%)' ;
-
+function showNav() {
+  navMenu.style.transform = "translateX(0%)";
 }
 
-function closeNav(){
-  navMenu.style.transform = 'translateX(-100%)';
+function closeNav() {
+  navMenu.style.transform = "translateX(-100%)";
 }
 
-navButton.addEventListener('click',showNav);
+navButton.addEventListener("click", showNav);
 
-navCloseButton.addEventListener('click', closeNav);
-
-
+navCloseButton.addEventListener("click", closeNav);
 
 // Search bar logic
 
-searchButton.addEventListener('click', function(){
-  searchBarDiv.style.transform = 'translateY(0%)';
-})
+// searchButton.addEventListener('click', function(){
+//   searchBarDiv.style.transform = 'translateY(0%)';
+// })
 
-closeSearchButon.addEventListener('click', function(){
-  searchBarDiv.style.transform = 'translateY(-100%)';
-})
+// closeSearchButon.addEventListener('click', function(){
+//   searchBarDiv.style.transform = 'translateY(-100%)';
+// })
 
-userInput.addEventListener('keyUp', function(event){
+// userInput.addEventListener('keyUp', function(event){
 
-})
-
- 
+// })
