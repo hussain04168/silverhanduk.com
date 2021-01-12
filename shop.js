00a81774eeb33ddb5037c120238d5e0c58611f42
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-01-12 21:06:53 +0000

    [AM]: declared filter variables

--- shop.js
+++ shop.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+let filterButton = document.querySelector(".filter-btn");
+let brandButton = document.querySelector(".brand-btn");
+let genderButton = document.querySelector(".gender-btn");
+let styleButton = document.querySelector(".style-btn");
+let filterSelection = document.querySelector(".filter-list");
+let brandFilter = document.querySelector(".brands-list");
+let genderFilter = document.querySelector(".gender-list");
+let styleFilter = document.querySelector(".style-list");
