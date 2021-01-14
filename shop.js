23f789fccb7fa1a5e239f5442b7a8327925e78e1
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-01-14 20:26:42 +0000

    [AM]: Added button effect

--- shop.js
+++ shop.js
@@ -6,19 +6,30 @@ let filterSelection = document.querySelector(".filter-list");
 let brandFilter = document.querySelector(".brands-list");
 let genderFilter = document.querySelector(".gender-list");
 let styleFilter = document.querySelector(".style-list");
+let applyFilterButton = document.querySelector(".apply-filter-btn");
+let genderItems = document.getElementsByClassName("gender-item");
+let brandItems = document.getElementsByClassName("brand-item");
+let styleItems = document.getElementsByClassName("style-item");
 
 filterButton.addEventListener("click", () => {
-  filterSelection.classList.toggle("hide");
+  filterSelection.style.transform = "translateY(0)";
+  applyFilterButton.style.transform = "translateY(0)";
 });
 
 brandButton.addEventListener("click", () => {
-  brandFilter.classList.toggle("hide");
+  for (let i = 0; i < brandItems.length; i++) {
+    brandItems[i].style.transform = "translateY(0)";
+  }
 });
 
 genderButton.addEventListener("click", () => {
-  genderFilter.classList.toggle("hide");
+  for (let i = 0; i < genderItems.length; i++) {
+    genderItems[i].style.transform = "translateY(0)";
+  }
 });
 
 styleButton.addEventListener("click", () => {
-  styleFilter.classList.toggle("hide");
+  for (let i = 0; i < styleItems.length; i++) {
+    styleItems[i].style.transform = "translateY(0)";
+  }
 });
