30961aecce93357cc0c35ef9eebacfac09516d21
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-05-11 19:22:02 +0100

    Added search bar logic to all pages

---
+++ searchBarContact.js
@@ -0,0 +1,40 @@
+let userInputC = document.querySelector(".user-input");
+
+userInputC.addEventListener("keyup", (event) => {
+  let userSearch = event.target.value.toLowerCase();
+  let brandCollection = document.querySelectorAll("[data-brand]");
+  let nameCollection = document.querySelectorAll("[data-name]");
+  let sizeCollection = document.querySelectorAll("[data-size]");
+
+  for (let i = 0; i < brandCollection.length; i++) {
+    let newBrandsCollection = brandCollection[i].getAttribute("data-brand");
+    let newNameCollection = nameCollection[i].getAttribute("data-name");
+    let newSizeCollection = sizeCollection[i].getAttribute("data-size");
+    let keyPressed = event.keyCode;
+    if (
+      newBrandsCollection.includes(userSearch) ||
+      newNameCollection.includes(userSearch) ||
+      newSizeCollection.includes(userSearch)
+    ) {
+      brandCollection[i].style.display = "flex";
+      nameCollection[i].style.display = "flex";
+      sizeCollection[i].style.display = "flex";
+    } else {
+      brandCollection[i].style.display = "none";
+      nameCollection[i].style.display = "none";
+      sizeCollection[i].style.display = "none";
+    }
+
+    if (
+      (userSearch === "" && keyPressed === 8) ||
+      keyPressed === 37 ||
+      keyPressed === 38 ||
+      keyPressed === 39 ||
+      keyPressed === 40
+    ) {
+      brandCollection[i].style.display = "none";
+      nameCollection[i].style.display = "none";
+      sizeCollection[i].style.display = "none";
+    }
+  }
+});
